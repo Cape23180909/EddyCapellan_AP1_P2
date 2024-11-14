4e2a5c60a3db805a5b4c618d546799106a55e4ea
Cape23180909 <samilgamer@outlook.com>
2024-11-14 11:14:08 -0400

    Agregando plantillas del servicio

--- Services/RegistroService.cs
+++ Services/RegistroService.cs
@@ -1,5 +1,8 @@
 ﻿using EddyCapellan_AP1_P2.DAL;
+using EddyCapellan_AP1_P2.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using System.Linq.Expressions;
 
 namespace EddyCapellan_AP1_P2.Services;
 
@@ -9,5 +12,57 @@ public class RegistroService(IDbContextFactory<Contexto> DbFactory)
     {
         await using var contexto = await DbFactory.CreateDbContextAsync();
         return await contexto.Registro.AnyAsync(r => r.Id == RegistroId);
+    }
+
+    public async Task<bool> Insertar(Registro registro)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Registro.Add(registro);
+        return await contexto.SaveChangesAsync() > 0;
+
+    }
+
+    public async Task<bool> Modificar(Registro registro)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Registro.Update(registro);
+        return await contexto.SaveChangesAsync() > 0;
+
+    }
+    public async Task<bool> Guardar(Registro registro)
+    {
+        if (!await Existe(registro.Id))
+            return await Insertar(registro);
+        else
+            return await Modificar(registro);
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        var eliminado = await contexto.Registro
+           .Where(r => r.Id == id)
+           .ExecuteDeleteAsync();
+        return eliminado > 0;
+
+    }
+
+    public async Task<Registro?> Buscar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Registro
+            .AsNoTracking()
+            .FirstOrDefaultAsync(r => r.Id == id);
+
+    }
+
+    public async Task<List<Registro>> Listar(Expression<Func<Registro, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Registro
+            .AsNoTracking()
+            .Where(criterio)
+            .ToListAsync();
+
     }
 }
